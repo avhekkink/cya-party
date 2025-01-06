a8119dbec4cc7ed44aacc24d9da2782884bd1a96
Anna Hekkink <anna.hekkink@medtronic.com>
2025-01-06 00:45:43 +0000

    add image optimization with vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+      svg: {
+        plugins: [
+          { name: "removeViewBox", active: false },
+          { name: "sortAttrs" },
+        ],
+      },
+    }),
+  ],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
